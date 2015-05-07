179f8ae6cc0d27e231e36f2605cec8a247b821b5
Alex Tuzhikov <at_alex@bk.ru>
2015-05-08 00:17:50 +0400

    7.05.2015

---
+++ TR/TestCountDown_en.ts
@@ -0,0 +1,412 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../dialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Окно вывода</translation>
+    </message>
+    <message>
+        <location filename="../dialog.ui" line="22"/>
+        <source>Value:</source>
+        <translation>Значение:</translation>
+    </message>
+    <message>
+        <location filename="../dialog.ui" line="29"/>
+        <source>#x 255 90 10 $95</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../dialog.ui" line="36"/>
+        <source>Send</source>
+        <translation>Отправить</translation>
+    </message>
+    <message>
+        <location filename="../dialog.ui" line="47"/>
+        <source>Add 0xOD</source>
+        <translation>Добавить 0x0D</translation>
+    </message>
+    <message>
+        <location filename="../dialog.ui" line="70"/>
+        <source>Add CRC</source>
+        <translation>Добавить CRC</translation>
+    </message>
+    <message>
+        <location filename="../dialog.ui" line="77"/>
+        <source>Exit</source>
+        <translation>Выход</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>MainWindow</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="60"/>
+        <source>Address board:</source>
+        <translation>Адрес табло:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="111"/>
+        <source>Auto Mode</source>
+        <translation>Автоматический режим</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="144"/>
+        <source>Countdown red light.</source>
+        <translation>Обратный отсчет красного сигнала.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="155"/>
+        <location filename="../mainwindow.ui" line="223"/>
+        <source>Set time countdown.</source>
+        <translation>Время обратного отсчета.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="177"/>
+        <location filename="../mainwindow.ui" line="245"/>
+        <location filename="../mainwindow.ui" line="343"/>
+        <location filename="../mainwindow.ui" line="428"/>
+        <source>m:ss</source>
+        <translation>m:ss</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="188"/>
+        <location filename="../mainwindow.ui" line="256"/>
+        <source>Set flashing time.</source>
+        <translation>Время мигания.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="212"/>
+        <source>Countdown green light.</source>
+        <translation>Обратный отсчет зеленого сигнала.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="296"/>
+        <source>Control Commands</source>
+        <translation>Команды управления</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="309"/>
+        <source>Enabled board, the indicator is red:   </source>
+        <translation>Включить красный индикатор:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="321"/>
+        <location filename="../mainwindow.ui" line="406"/>
+        <source>Time countdown</source>
+        <translation>Время отчета</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="354"/>
+        <location filename="../mainwindow.ui" line="439"/>
+        <source>Blinking</source>
+        <translation>Мигание</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="397"/>
+        <source>Enabled board, the indicator is green:</source>
+        <translation>Включить зеленый индикатор: </translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="480"/>
+        <source>Disable board.</source>
+        <translation>Отключить табло.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="492"/>
+        <source>Manual control.</source>
+        <translation>Ручное управление.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="504"/>
+        <source>Adaptive control.</source>
+        <translation>Адаптивное управление.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="516"/>
+        <source>Supervisory control.</source>
+        <translation>Диспетчерское управление.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="537"/>
+        <source>Services Commands</source>
+        <translation>Сервисные команды</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="547"/>
+        <source>Get the address:</source>
+        <translation>Считать адрес:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="559"/>
+        <source>Set the address:</source>
+        <translation>Установить адрес:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="648"/>
+        <source>Set flashing time:</source>
+        <translation>Уст. время мигания:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="655"/>
+        <source>Set current:</source>
+        <translation>Установить ток:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="662"/>
+        <source>Set the brightness:</source>
+        <translation>Установить яркость:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="673"/>
+        <source>Test 1:</source>
+        <translation>Тест 1:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="680"/>
+        <source>Test 2:</source>
+        <translation>Тест 2:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="689"/>
+        <source>brightness</source>
+        <translation>яркость</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="754"/>
+        <source>Manual</source>
+        <translation>Ручной</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="799"/>
+        <source>Start\Set</source>
+        <translation>Старт\Отправить</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="812"/>
+        <source>Stop</source>
+        <translation>Стоп</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="851"/>
+        <source>&amp;File</source>
+        <translation>&amp;Файл</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="857"/>
+        <source>&amp;Setting</source>
+        <translation>&amp;Установка</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="869"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Помощь</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="875"/>
+        <source>&amp;View</source>
+        <translation>&amp;Вид</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="904"/>
+        <source>&amp;Exit</source>
+        <translation>&amp;Выход</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="913"/>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="922"/>
+        <source>USB RS485</source>
+        <translation>USB RS485</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="933"/>
+        <source>Basic mode</source>
+        <translation>Базовый режим</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="941"/>
+        <source>Terminal mode</source>
+        <translation>Терминальный режим</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="952"/>
+        <source>HEX value</source>
+        <translation>HEX значение</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="960"/>
+        <source>DEC value</source>
+        <translation>DEC значение</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="971"/>
+        <source>&amp;Log</source>
+        <translation>&amp;Окно вывода</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="46"/>
+        <source>version 1.2</source>
+        <translation>версия 1.2</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="49"/>
+        <source>Test CountDown</source>
+        <translation>Табло обратного отчета</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="114"/>
+        <source>About the program, a countdown</source>
+        <translation>О программе, табло обратного отсчета</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="115"/>
+        <source>&lt;h2&gt; Test countdown board &apos;TOO&apos; version 1.2&lt;/h2&gt;&lt;p&gt;Co., Ltd. Elintel &lt;a href=&quot;http://www.elintel.ru/&quot;&gt;www.elintel.ru&lt;/a&gt; &lt;/p&gt;&lt;p&gt;The program countdown.</source>
+        <translation>&lt;h2&gt; Тест табло обратного отсчета версия 1.2&lt;/h2&gt;&lt;p&gt;ООО &quot;Элинтел&quot; &lt;a href=&quot;http://www.elintel.ru/&quot;&gt;www.elintel.ru&lt;/a&gt; &lt;/p&gt;&lt;p&gt;Программа табло обратного отсчета.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="220"/>
+        <source>Message Error</source>
+        <translation>Сообщение об ошибке</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="235"/>
+        <source>Identification USB devices</source>
+        <translation>Идентификация USB устройства</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="236"/>
+        <source>Available USB device</source>
+        <translation>Доступные устройства USB</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="247"/>
+        <source>USB failed</source>
+        <translation>Ошибка USB</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="247"/>
+        <source>USB/RS485 not found!</source>
+        <translation>USB/RS485 не определен!</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="18"/>
+        <source>testCountDown</source>
+        <translation>Тест табло обратного отсчета</translation>
+    </message>
+</context>
+<context>
+    <name>TParserXML</name>
+    <message>
+        <location filename="../parserxml.cpp" line="123"/>
+        <location filename="../parserxml.cpp" line="134"/>
+        <source>Configuration File</source>
+        <translation>Файл конфигурации</translation>
+    </message>
+    <message>
+        <location filename="../parserxml.cpp" line="124"/>
+        <source>This file does not configure the device!</source>
+        <translation>Ошибка в файле настройки!</translation>
+    </message>
+    <message>
+        <location filename="../parserxml.cpp" line="135"/>
+        <source>Error on line %1, column %2:
+%3!</source>
+        <translation>Ошибка в строке %1, в столбце %2:
+%3!</translation>
+    </message>
+</context>
+<context>
+    <name>ftdiChip</name>
+    <message>
+        <location filename="../ftdichip.cpp" line="39"/>
+        <source>&lt;CENTER&gt;&lt;b&gt;Not found KIT USN_RS485!&lt;/CENTER&gt;&lt;/b&gt;</source>
+        <translation>&lt;CENTER&gt;&lt;b&gt; Не найден КИТ USN_RS485!&lt;/CENTER&gt;&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ftdichip.cpp" line="94"/>
+        <source>&lt;CENTER&gt;&lt;b&gt;Data is written successfully!&lt;/CENTER&gt;&lt;/b&gt;</source>
+        <translation>&lt;CENTER&gt;&lt;b&gt;Данные записались успешно!&lt;/CENTER&gt;&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ftdichip.cpp" line="96"/>
+        <source>&lt;CENTER&gt;&lt;b&gt;Error response!&lt;/CENTER&gt;&lt;/b&gt;</source>
+        <translation>&lt;CENTER&gt;&lt;b&gt;Ошибка ответа!&lt;/CENTER&gt;&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ftdichip.cpp" line="98"/>
+        <source>&lt;CENTER&gt;&lt;b&gt;Device does not answer!&lt;/CENTER&gt;&lt;/b&gt;</source>
+        <translation>&lt;CENTER&gt;&lt;b&gt;Устройство не отвечает!&lt;/CENTER&gt;&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ftdichip.cpp" line="100"/>
+        <source>&lt;CENTER&gt;&lt;b&gt;Data is incorrect!&lt;/CENTER&gt;&lt;/b&gt;</source>
+        <translation>&lt;CENTER&gt;&lt;b&gt;Неверные данные!&lt;/CENTER&gt;&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ftdichip.cpp" line="218"/>
+        <source>Error open USB</source>
+        <translation>Ошибка открытия USB</translation>
+    </message>
+    <message>
+        <location filename="../ftdichip.cpp" line="223"/>
+        <source>Error setting buffer sizes USB</source>
+        <translation>Ошибка установки размера буфера USB</translation>
+    </message>
+    <message>
+        <location filename="../ftdichip.cpp" line="266"/>
+        <source>Error writing data to USB</source>
+        <translation>Ошибка записи данных в USB</translation>
+    </message>
+    <message>
+        <location filename="../ftdichip.cpp" line="304"/>
+        <location filename="../ftdichip.cpp" line="313"/>
+        <source>USB write error status</source>
+        <translation>Ошибка записи статуса USB</translation>
+    </message>
+    <message>
+        <location filename="../ftdichip.cpp" line="333"/>
+        <source>Error setting parameters &lt;speed FTDI&gt;</source>
+        <translatorcomment>Ошибка установки параметров &lt;speed FTDI&gt;</translatorcomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ftdichip.cpp" line="339"/>
+        <source>Error setting parameters &lt;databit FTDI&gt;</source>
+        <translation>Ошибка установки параметров &lt;databit FTDI&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ftdichip.cpp" line="345"/>
+        <source>Parameters is successful!</source>
+        <translation>Параметры установлены!</translation>
+    </message>
+    <message>
+        <location filename="../ftdichip.cpp" line="370"/>
+        <source>Failed to open USB port</source>
+        <translation>Не удалось открыть порт USB</translation>
+    </message>
+    <message>
+        <location filename="../ftdichip.cpp" line="384"/>
+        <location filename="../ftdichip.cpp" line="390"/>
+        <source>Error writing flash</source>
+        <translation>Ошибка записи флэш</translation>
+    </message>
+    <message>
+        <location filename="../ftdichip.cpp" line="387"/>
+        <source>Flash is successfully</source>
+        <translation>Флешь записана</translation>
+    </message>
+</context>
+</TS>
